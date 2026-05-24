5b01c0e61019404a396658e52b2c30e1bd91b34a
Respresso <respresso@RESPRESSO.dlink>
2026-05-24 13:04:52 +0300

    add api from linux

--- supabase_config.js
+++ supabase_config.js
@@ -1,7 +1,7 @@
 // Конфигурация Supabase
 // ЗАМЕНИТЕ ЭТИ ЗНАЧЕНИЯ НА ВАШИ ИЗ ЛИЧНОГО КАБИНETA SUPABASE
-export const SUPABASE_URL = 'https://your-project-id.supabase.co';
-export const SUPABASE_ANON_KEY = 'your-anon-key';
+export const SUPABASE_URL = https://bqaejvutckdthcgxzwje.supabase.co/rest/v1/;
+export const SUPABASE_ANON_KEY = sb_publishable_PrLnYTbErX-nMixZcvdvbA_GNz_7bES;
 
 // Название таблицы в базе данных
 export const TASKS_TABLE = 'tasks';
