f6a2ea17d77b924321534a08575101047dc2a38d
Respresso <respresso@RESPRESSO.dlink>
2026-06-02 23:34:16 +0300

    add .evn

--- supabase_config.js
+++
@@ -1,7 +0,0 @@
-// Конфигурация Supabase
-// ЗАМЕНИТЕ ЭТИ ЗНАЧЕНИЯ НА ВАШИ ИЗ ЛИЧНОГО КАБИНETA SUPABASE
-export const SUPABASE_URL = 'https://bqaejvutckdthcgxzwje.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJxYWVqdnV0Y2tkdGhjZ3h6d2plIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3OTIzNzE4OCwiZXhwIjoyMDk0ODEzMTg4fQ.jJo46GmswMSn4_wzIyzgHBPj-atL9wADm4RnYQhWuIQ';
-
-// Название таблицы в базе данных
-export const TASKS_TABLE = 'tasks';
